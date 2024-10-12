0218e37b52bd58d5e751eae4e26f94252e5e7c48
merytpeters <merytpeters@gmail.com>
2024-10-12 11:18:09 +0100

    Background script, that checks tab inactivity

--- background.js
+++ background.js
@@ -1,17 +1,19 @@
 const tabLastAccessed = {};
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
   const tabId = activeInfo.tabId;
   const timestamp = Date.now();
 
   tabLastAccessed[tabId] = timestamp;
+  await chrome.storage.local.set({tabLastAccessed});
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status === 'complete') {
     const timestamp = Date.now();
 
     tabLastAccessed[tabId] = timestamp;
+    chrome.storage.local.set({ tabLastAccessed });
   }
 });
 
@@ -19,6 +21,13 @@ chrome.tabs.onRemoved.addListener((tabId) => {
   delete tabLastAccessed[tabId];
 });
 
-function getLastAccessed(tabId) {
+async function initialize() {
+  const { tabLastAccessed: storedData = {} } = await chrome.storage.local.get('tabLastAccessed');
+  Object.assign(tabLastAccessed, storedData);
+}
+
+initialize();
+
+async function getLastAccessed(tabId) {
   return tabLastAccessed[tabId] || null;
 }
